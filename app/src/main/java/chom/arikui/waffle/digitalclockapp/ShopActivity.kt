eb4d2764c524b962cadcefe518c0139189123bc4
Y-Joron <yjoron231@gmail.com>
2021-09-23 07:19:05 +0900

    ショップ画面、文字の大きさなど調整。

--- app/src/main/java/chom/arikui/waffle/digitalclockapp/ShopActivity.kt
+++ app/src/main/java/chom/arikui/waffle/digitalclockapp/ShopActivity.kt
@@ -1,6 +1,8 @@
 package chom.arikui.waffle.digitalclockapp
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import chom.arikui.waffle.digitalclockapp.databinding.ShopActivityBinding
@@ -12,5 +14,21 @@ class ShopActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ShopActivityBinding>(
             this, R.layout.shop_activity
         )
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        hideSystemUI()
+        window.addFlags(
+            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        binding.buttonUpgrade.setOnClickListener {
+
+        }
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
     }
 }
